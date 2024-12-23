3accc94c06f933b33ae3aad2506180443ab396a7
silmidina <silmirhmdn08@gmail.com>
2024-12-23 10:18:42 +0700

    about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
     extend: {
       colors: {
         primary: '#67e8f9',
-        dark: '#0f172a'
+        dark: '#0f172a',
+        secondary: '#64748b',
+        lime: '#84cc16'
       },
       screens: {
         '2xl': '1320px',
